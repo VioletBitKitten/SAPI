32b09af41afef91ce26446a0f10dd21fa3c263bb
Violet Bit Kitten <>
2023-08-01 22:46:51 -0700

    Undo moving source to a subdirectory.
    The Free Pascal make on Windows has too many issues. Leaving the source in the main directory works better.

--- build.bat
+++ build.bat
@@ -33,8 +33,6 @@ REM Cleanup build artifacts, including the Makefile.
     fpcmake
     make distclean
     DEL Makefile
-    REM Because Freepascal Make on Windows is a broken mess...
-    DEL runtests.exe tts.exe ttsdemo.exe
 GOTO exitscript
 
 REM Show some help text.
